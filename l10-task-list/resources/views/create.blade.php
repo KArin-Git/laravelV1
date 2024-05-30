f4439fbc5055b9e2ed33281bfa7d9f1f19f3fb16
Arin <arin.apichitchon@gmail.com>
2024-05-29 23:16:12 -0400

    reusing blade - subview

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -1,40 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Add a Task')
-@section('styles')
-    <style>
-        .error-msg {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-@section('content')
-    {{-- {{ $errors }} --}}
-    <form method="POST" action="{{ route('tasks.store') }}">
-        @csrf
-        <div>
-            <label for="title">Title</label>
-            {{-- old helper work with form method"POST" only NOT PROOFING show the value that store in session --}}
-            <input type="text" name="title" id="title" value="{{ old('title') }}">
-            @error('title')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="desc">Description</label>
-            <textarea name="description" id="desc" cols="15" rows="5">{{ old('description') }}</textarea>
-            @error('description')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="long_desc">Long Description</label>
-            <textarea name="long_description" id="long_desc" cols="15" rows="10">{{ old('long_description') }}</textarea>
-            @error('long_description')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <button>Add task</button>
-    </form>
 
+@section('content')
+    @include('form')
 @endsection
--- l10-task-list/resources/views/edit.blade.php
+++ l10-task-list/resources/views/edit.blade.php
@@ -1,42 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Edit a Task')
-@section('styles')
-    <style>
-        .error-msg {
-            color: red;
-            font-size: 0.8rem;
-        }
-    </style>
-@endsection
-@section('content')
-    {{-- {{ $errors }} --}}
-    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
-        @csrf
-        {{-- directive method bc http don't have put >> spoofing --}}
-        @method('PUT')
-        <div>
-            <label for="title">Title</label>
-            {{--  add value="{{ $task->title }}" to called old value --}}
-            <input type="text" name="title" id="title" value="{{ $task->title }}">
-            @error('title')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="desc">Description</label>
-            <textarea name="description" id="desc" cols="15" rows="5">{{ $task->description }}</textarea>
-            @error('description')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <div>
-            <label for="long_desc">Long Description</label>
-            <textarea name="long_description" id="long_desc" cols="15" rows="10">{{ $task->long_description }}</textarea>
-            @error('long_description')
-                <p class="error-msg">{{ $message }}</p>
-            @enderror
-        </div>
-        <button>Add task</button>
-    </form>
 
+@section('content')
+    @include('form', ['task' => $task])
 @endsection
