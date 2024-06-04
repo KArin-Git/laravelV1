8d78fd74542eea2c6c3a4804a1d28487baa9a14b
Arin <arin.apichitchon@gmail.com>
2024-06-03 22:17:51 -0400

    add tailwind

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -2,21 +2,22 @@
 
 @section('title', 'The List of Tasks')
 @section('content')
-    <div>
-        <a href="{{ route('tasks.create') }} ">Add Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="link">Add Task</a>
+    </nav>
     <div>
         @forelse($tasks as $task)
             <div>
-                <a href="{{ route('tasks.show', ['task' => $task->id]) }}">
-                    {{ $task->title }}
+                <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->complete])>{{ $task->title }}
                 </a>
             </div>
         @empty
             <div>There are NO tasks.</div>
         @endforelse
         @if ($tasks->count())
-            {{ $tasks->links() }}
+            <nav class="mt-4">
+                {{ $tasks->links() }}
+            </nav>
         @endif
     </div>
 @endsection
--- l10-task-list/resources/views/show.blade.php
+++ l10-task-list/resources/views/show.blade.php
@@ -2,29 +2,35 @@
 
 @section('title', $task->title)
 @section('content')
-    <p> {{ $task->description }}</p>
-    @if ($task->long_description)
-        <p> {{ $task->long_description }}</p>
-    @endif
-    <p>{{ $task->created_at }}</p>
-    <p>{{ $task->updated_at }}</p>
-    <p>{{ $task->complete ? 'Completed' : 'Not Completed' }}</p>
-    <div>
-        {{-- no need to specific $task->id as laravel known that it should select with prime key --}}
-        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}" class="link">Go back to the task list</a>
     </div>
-    <div>
+    <p class="mb-4 text-slate-700"> {{ $task->description }}</p>
+    @if ($task->long_description)
+        <p lass="mb-4 text-slate-700"> {{ $task->long_description }}</p>
+    @endif
+    <p class="mb-4 text-sm text-slate-500">Created {{ $task->created_at->diffForHumans() }} . Updated
+        {{ $task->updated_at->diffForHumans() }}</p>
+    <p class="mb-4">
+        @if ($task->complete)
+            <span class="font-medium text-green-500">Completed</span>
+        @else
+            <span class="font-medium text-red-500">Not Completed</span>
+        @endif
+    <div class="flex gap-2">
+        {{-- no need to specific $task->id as laravel known that it should select with prime key --}}
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}" class="btn">Edit</a>
+
         <form method="post" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
             @csrf
             @method('put')
-            <button type="submit">Mark as {{ $task->complete ? 'not completed' : 'completed' }}</button>
+            <button type="submit" class="btn">Mark as {{ $task->complete ? 'not completed' : 'completed' }}</button>
         </form>
-    </div>
-    <div>
+
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             @csrf
             @method('delete')
-            <button type="submit">delete</button>
+            <button type="submit" class="btn">delete</button>
         </form>
     </div>
 @endsection
