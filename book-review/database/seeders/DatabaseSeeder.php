fc83e17b0c4edc70a2e7f249ab9fd43f0226145e
Arin <arin.apichitchon@gmail.com>
2024-06-05 23:23:42 -0400

    migrate:refresh --seed to add books and reviews

--- book-review/database/seeders/DatabaseSeeder.php
+++ book-review/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Book;
+use App\Models\Review;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // create 33 books and for each book create random number of reviews
+        Book::factory(33)->create()->each(function ($book) {
+            // how many reviews we want from particular book
+            $numReviews = random_int(5, 39);
+            // generate reviews for each book
+            Review::factory()->count($numReviews)
+                ->good()
+                // book_id will be set automatically
+                ->for($book)
+                ->create();
+        });
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 39);
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 39);
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
