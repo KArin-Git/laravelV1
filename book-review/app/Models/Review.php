6b5cab9a82c27bae3189a5aef57439a19281f756
Arin <arin.apichitchon@gmail.com>
2024-06-23 15:25:04 -0400

    fixed FIXME: data not updated in cache by adding booted() in Review Model

--- book-review/app/Models/Review.php
+++ book-review/app/Models/Review.php
@@ -16,4 +16,15 @@ class Review extends Model
         // default column setup from Laravel >> class_id
         // return $this->belongsTO(Book::class, 'book_id', 'id');
     }
+
+    protected static function booted() {
+        // if the review is updated, then clear the cache of the book
+        // whenever this review's model is modified, then this function will be called
+        // **this happen to the model. It CANT be done directly inside db
+        static::updated(fn (Review $review) => cache()->forget('book:' . $review->book_id));
+        static::deleted(fn (Review $review) => cache()->forget('book:' . $review->book_id));
+        
+        // MARKME: this handler is not be called in every situation.
+        // if we use mass assignment which means we update multiple rows at once, then this handler will not be called >> $review->book_id
+    }
 }
